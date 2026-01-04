7ab7f8cb1fe472e95e2408e4a00b3de77f988b1e
Enthusiasticperson <enthusiasticperson@gmail.com>
2026-01-04 21:51:33 +0530

    sl2

--- js/multiMarkersName.js
+++ js/multiMarkersName.js
@@ -21,7 +21,7 @@ AFRAME.registerComponent('markers_start',{
 
 		// Single marker setup
 		// markersURLArray.push("resources/markers/target.patt");
-		markersURLArray.push("pattern-Individual_Blocks-1.patt");
+		markersURLArray.push("resources/markers/target.patt");
 		markersNameArray.push("D5-A2");
 
 		for(var k=0; k<1; k++)
